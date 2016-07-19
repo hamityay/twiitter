6f6ee5f2081bdded143e675c72acce5c5159b34e
Hamit YAY <hamityay@hotmail.com>
2016-07-19 11:01:46 +0300

    Add user name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719063003) do
+ActiveRecord::Schema.define(version: 20160719070606) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "tweet_id"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20160719063003) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
